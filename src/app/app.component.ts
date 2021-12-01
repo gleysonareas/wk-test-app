f7f6ddba12515bf30c47bd33c52795add0d033f2
Gleyson (Easy) <gleyson@easyteam.rocks>
2021-12-01 20:49:16 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wkt-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'wk-test-app';
+}
