ee74e34ffc6eccfb17d9cd1c44bed58a3dc10db8
Gleyson (Easy) <gleyson@easyteam.rocks>
2021-12-10 00:43:47 -0300

    modifications necessary for the test app

--- src/components/side-nav/side-nav.component.ts
+++ src/components/side-nav/side-nav.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SideNavComponent implements OnInit {
 
   @Input() 
-  public showFiller = true;
+  public showFiller = false;
 
   constructor() { }
 
